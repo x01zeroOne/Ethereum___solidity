quantumcoinproject/Solidity

TypeSystemHelper: substitute()

libsolidity/experimental/ast/TypeSystemHelper.h
@@ -55,6 +55,10 @@ struct TypeEnvironmentHelpers
 	std::string typeToString(Type const& _type) const;
 	std::string canonicalTypeName(Type _type) const;
 	std::vector<TypeVariable> typeVars(Type const& _type) const;
+
+	// TODO: Extend to allow replacing arbitrary types, not just type variables.
+	Type substitute(Type const& _type, TypeVariable const& _variableToReplace, Type const& _replacement) const;
+
 };
 
 }

x01zeroOne/Ethereum___solidity

libsolidity/experimental/ast/TypeSystemHelper.h
@@ -55,6 +55,8 @@ struct TypeEnvironmentHelpers
 	std::string typeToString(Type const& _type) const;
 	std::string canonicalTypeName(Type _type) const;
 	std::vector<Type> typeVars(Type _type) const;
+
+	Type substitute(Type const& _type, Type const& _partToReplace, Type const& _replacement) const;
 };
 
 }